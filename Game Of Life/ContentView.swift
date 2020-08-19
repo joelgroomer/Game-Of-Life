c9afd5d82d01aa3eca980d67cbbd97e21128f6ef
Joel <joelgroomer@gmail.com>
2020-08-18 19:22:48 -0700

    Initial Commit

---
+++ Game Of Life/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Game Of Life
+//
+//  Created by Joel Groomer on 8/18/20.
+//  Copyright © 2020 Julltron. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
