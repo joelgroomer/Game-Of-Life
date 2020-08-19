8fc4de0fe721e9f627e75f752fd08e1ce5ac0a5d
Joel <joelgroomer@gmail.com>
2020-08-18 19:54:34 -0700

    Initial commit

---
+++ Game Of Life/Views/ContentView.swift
@@ -0,0 +1,40 @@
+//
+//  ContentView.swift
+//  Game Of Life
+//
+//  Created by Joel Groomer on 8/18/20.
+//  Copyright © 2020 Julltron. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        TabView {
+            
+            Text("Hello, World!")
+            .tabItem {
+                Image(systemName: "square.grid.2x2")
+                Text("Simulation")
+            }
+            Text("Options")
+            .tabItem {
+                Image(systemName: "slider.horizontal.3")
+                Text("Options")
+            }
+            Text("Rules")
+            .tabItem {
+                Image(systemName: "questionmark.circle")
+                Text("Rules")
+            }
+            
+        }
+        .font(.headline)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
