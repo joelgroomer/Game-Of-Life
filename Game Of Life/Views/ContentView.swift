9dc155a2aadacb65962cde6459d8aecee7e4439b
Joel <joelgroomer@gmail.com>
2020-08-19 19:26:57 -0700

    Changed layout to single screen

--- Game Of Life/Views/ContentView.swift
+++ Game Of Life/Views/ContentView.swift
@@ -9,27 +9,43 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var selectedSeg = 0
+    @State private var gridScaleCurrent: CGFloat = 0
+    @State private var gridScaleFinal: CGFloat = 1
+    
     var body: some View {
-        TabView {
+        
+        HStack {
+            ZStack {
+                Color(.lightGray)
+                GridView()
+                    .padding()
+                    .scaleEffect(gridScaleFinal + gridScaleCurrent)
+                    .gesture(
+                        MagnificationGesture()
+                            .onChanged { amount in
+                                self.gridScaleCurrent = amount - 1
+                            }
+                            .onEnded { amount in
+                                self.gridScaleFinal += self.gridScaleCurrent
+                                self.gridScaleCurrent = 0
+                            }
+                    )
+            }
             
-            GridView()
-            .tabItem {
-                Image(systemName: "square.grid.2x2")
-                Text("Simulation")
+            ZStack {
+                Color(.systemBlue)
+                VStack {
+                    Picker(selection: $selectedSeg,
+                           label: Text("menu")) {
+                            Text("Options").tag(0)
+                            Text("Rules").tag(1)
+                    }.pickerStyle(SegmentedPickerStyle())
+                    Text("Selection: \(selectedSeg)")
+                    Spacer()
+                }
             }
-            Text("Options")
-            .tabItem {
-                Image(systemName: "slider.horizontal.3")
-                Text("Options")
-            }
-            Text("Rules")
-            .tabItem {
-                Image(systemName: "questionmark.circle")
-                Text("Rules")
-            }
-            
         }
-        .font(.headline)
     }
 }
 
