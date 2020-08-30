527ae499fb8e20c9662a0edc369c768eaccfa195
Joel <joelgroomer@gmail.com>
2020-08-30 12:16:28 -0700

    Updated rules screen Better explanation of what constitutes a neighbor, link to Wikipedia article about Conway's Game of Life

--- Game Of Life/Views/RulesView.swift
+++ Game Of Life/Views/RulesView.swift
@@ -36,10 +36,18 @@ When the simulation is started, 'generations' begin to progress. Each generation
 2. A live cell with less than two or more than three living neighbors dies.
 3. A dead cell with three living neighbors becomes a living cell in the next generation.
 4. A dead cell with any other number of living neighbors remains dead.
-
-"Neighbors" here means any of the 8 adjacent cells. On the "Options" tab, you have the ability to apply variations to the rules to see how they effect the game of life.
+"""
+                     )
+                Image("cellneighbors")
+                Text("""
+"Neighbors" here means any of the 8 adjacent cells. In the image above, the center (empty) cell has eight neighbors (the filled cells around it). A corner cell only has three neighbors, whereas a cell against one of the walls of the grid will have only five. On the "Options" tab, you have the ability to apply variations to the rules to see how they effect the game of life.
 """
                 )
+                Spacer()
+                Link("Tap here to learn more about Conway's Game of Life", destination: URL(string: "https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life")!)
+                    .foregroundColor(.white)
+                    .padding()
+                    .border(Color.white)
             }.padding()
         }
     }
